e0393a8b42902f9fa9a37de242fe839143c55ee2
George Taveras <george.taveras1231@gmail.com>
2017-01-30 13:06:34 -0500

    Update hack for typography class composition

--- packages/core/icons/src/index.js
+++ packages/core/icons/src/index.js
@@ -1,10 +1,27 @@
 var styles = require('icons');
+var builder = require('css-module-builder');
 
-for ( var key in styles.locals ) {
- if (key !== 'icon' && key.indexOf('icon') > -1) {
-   /* Hack to compose icon in all icon classes without actually doing it ;) */
-   styles.locals[key] += ' ' + styles.locals['icon'];
- }
+var wrapperModule = builder.initialize(styles[0][0], styles.toString());
+
+var locals = styles.locals;
+
+for (var key in locals) {
+  var values =  [
+    builder.getLocal(styles, key)
+  ];
+
+  if (key !== 'icon' && key.indexOf('icon') > -1) {
+    /* Hack to compose icon in all icon classes without actually doing it ;) */
+    values.push(builder.getLocal(styles, 'icon'));
+  }
+
+  wrapperModule.defineLocals([
+    [
+      [key],
+      values
+    ]
+  ]);
 }
 
-module.exports = styles;
+
+module.exports = wrapperModule;
