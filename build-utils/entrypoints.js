366ffbd45ca4b6ed494341b9715263a56bdc96ac
George Taveras <george.taveras1231@gmail.com>
2017-02-10 21:09:48 -0500

    Create entnrypoint utility

---
+++ build-utils/entrypoints.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const glob = require('glob-all');
+
+function defaultExtensionMapper(extension) {
+  if (/\.jsx?$/.test(extension)) {
+    return '.js';
+  }
+
+  if (/\.s?css$/.test(extension)) {
+    return '.cssm';
+  }
+
+  return extension;
+}
+
+exports.getEntrypointsFiles = function getEntrypointsFiles({ where }) {
+  return glob.sync([
+    '**/.entrypoints.json'
+  ], {
+    cwd: where,
+    absolute: true
+  });
+};
+
+exports.adaptEntrypoints = function adaptEntrypoints(
+  entrypointFiles,
+  {
+    relativeTo,
+    mapExtension = defaultExtensionMapper
+  }
+) {
+  return entrypointFiles.reduce((allEntrypoints, entrypointFile) => {
+    const files = require(entrypointFile);
+    const relativePath = path.relative(relativeTo, entrypointFile);
+    const relativeDir = path.dirname(relativePath);
+
+    files.forEach((file) => {
+      const extension = path.extname(file);
+      const basename = path.basename(file, extension);
+      const dirname = path.dirname(file);
+      const newExtension = mapExtension(extension);
+
+      const destinationPath = path.join(relativeDir, dirname, `${basename}${newExtension}`);
+      const sourcePath = path.join(relativeDir, file);
+
+      allEntrypoints[destinationPath] = `./${sourcePath}`;
+    });
+
+    return allEntrypoints;
+  }, {});
+};
+
+exports.buildEntrypoints = function buildEntrypoints({ context }) {
+  const entrypointFiles = exports.getEntrypointsFiles({ where: context });
+  return exports.adaptEntrypoints(entrypointFiles, { relativeTo: context });
+};
