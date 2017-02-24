03b625937c399e9e8788dd497328bba80b7c36eb
George Taveras <george.taveras1231@gmail.com>
2017-02-24 16:52:39 -0500

    Make blue header nav

--- pattern-library/docs/src/components/HeaderNav/index.jsx
+++ pattern-library/docs/src/components/HeaderNav/index.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import bsUtils from '@xo-union/bootstrap/utilities';
+import { Link } from 'react-router';
+import bsNav from '@xo-union/bootstrap/nav';
+
 import logo from '#docs/images/tk-logo.svg';
 import styles from './styles.css';
 
+
+function NavItem({ to, children }) {
+  return (
+    <Link to={to} className={styles.navLink}>{children}</Link>
+  );
+}
+
+function Nav({ children }) {
+  return (
+    <nav className={bsNav.nav}>
+      {children}
+    </nav>
+  );
+}
+
 export default function HeaderNav() {
   return (
-    <header className={styles.header}>
-      <a className={styles.link} href="/">
-        <img alt="the knot logo" src={logo} height="40" className={bsUtils.alignBaseline} />
-        <span>Union</span>
-      </a>
+    <header className={styles.headerNav}>
+      <Nav>
+        <NavItem to="/">The Knot Pattern Library</NavItem>
+        <NavItem to="/getting-started">Getting Started</NavItem>
+        <NavItem to="/core-components">Core Components</NavItem>
+        <NavItem to="/content-patterns">Content Patterns</NavItem>
+      </Nav>
     </header>
   );
 }
