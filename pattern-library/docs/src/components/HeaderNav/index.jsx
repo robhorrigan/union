9127aee13c92161b5c45ec5f77174ab673328e50
George Taveras <george.taveras1231@gmail.com>
2017-02-25 17:10:05 -0500

    Add arrow next to main link in HeaderNav

--- pattern-library/docs/src/components/HeaderNav/index.jsx
+++ pattern-library/docs/src/components/HeaderNav/index.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import logo from '#docs/images/tk-logo.svg';
 import styles from './styles.css';
 import { Nav, NavItem } from './Nav';
+import { Arrow } from './Icons';
 
 export default class HeaderNav extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class HeaderNav extends Component {
       <header className={styles.headerNav}>
         <Nav>
           <NavItem to="/">
+            <Arrow />
             The Knot Pattern Library
           </NavItem>
           <NavItem to="/getting-started">Getting Started</NavItem>
