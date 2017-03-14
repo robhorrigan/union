77804eaba3479c9c3981f8255228184ff5e9559f
George Taveras <george.taveras1231@gmail.com>
2017-03-14 17:01:42 -0400

    Add spec for toggleable mixin

---
+++ pattern-library/spec/docs/mixins/toggleable-spec.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { mount } from 'enzyme';
+import toggleable from '#docs/mixins/toggleable';
+import Toggler from '#docs/mixins/toggleable/toggler';
+import { Provider } from 'mobx-react';
+
+function getAttribute(...args) {
+  return this.getDOMNode().getAttribute(...args);
+}
+
+describe('@toggleable', () => {
+  @toggleable
+  class TestComponent extends Component {
+    render() {
+      return <div>Hello</div>;
+    }
+  }
+  
+  it('sets the id on the element', () => {
+    const toggler = new Toggler();
+    const subject = mount(
+      <TestComponent toggler={toggler} toggledAs="test-component-name" />
+    );
+    
+    expect(subject::getAttribute('id')).toBe('test-component-name');
+  });
+  
+  it('toggles the element when the name is added to the toggler', () => {
+    const toggler = new Toggler();
+    const subject = mount(
+      <TestComponent toggler={toggler} toggledAs="test-component-name" />
+    );
+    
+    expect(subject::getAttribute('data-toggle')).toBe('off');
+    expect(subject::getAttribute('aria-hidden')).toBe('true');
+    
+    toggler.show('test-component-name');
+    
+    expect(subject::getAttribute('data-toggle')).toBe('on');
+    expect(subject::getAttribute('aria-hidden')).toBe('false');
+  });
+});
