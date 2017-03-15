ac4b627ee4317adc68a8787fa43d9473eba518ae
George Taveras <george.taveras1231@gmail.com>
2017-03-15 19:34:04 -0400

    Create essetials package to help bootstrap apps

--- pattern-library/docs/src/index.jsx
+++ pattern-library/docs/src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from '#docs/components/Routes';
 
-/* Import bootstrap css globals */
-import '@xo-union/bootstrap';
-import '@xo-union/typography/globals';
+/* Global union styles */
+import '@xo-union/essentials';
+
 /* Load prism css for syntax highlighting */
 import 'prism-themes/themes/prism-ghcolors.css';
 
