c75a9f1109c46b6909322bf9cf3ddbab3c597b74
George Taveras <george.taveras1231@gmail.com>
2017-04-05 18:23:13 -0400

    Add links to header-nav

--- pattern-library/src/header-nav/index.jsx
+++ pattern-library/src/header-nav/index.jsx
@@ -2,23 +2,51 @@ import React, { PropTypes } from 'react';
 import styles from '@xo-union/header-nav/css';
 
 const topLevelItems = [
-  'Local Vendor',
-  'Wedding Website',
-  'Registry',
-  'Rings + Dresses',
-  'Photos',
-  'Ideas & Advice',
-  'Shop'
+  {
+    text: 'Local Vendor',
+    url: ''
+  },
+  {
+    text: 'Wedding Website',
+    url: ''
+  },
+  {
+    text: 'Registry',
+    url: ''
+  },
+  {
+    text: 'Rings + Dresses',
+    url: ''
+  },
+  {
+    text: 'Photos',
+    url: ''
+  },
+  {
+    text: 'Ideas & Advice',
+    url: ''
+  },
+  {
+    text: 'Shop',
+    url: ''
+  }
 ];
 
 export default function HeaderNav({ }) {
-  const listItems = topLevelItems.map((text) => {
-    return <li key={text} className={styles['top-level-item']}>{text}</li>;
-  });
+  const listItems = topLevelItems.map(({ text, url }) =>
+    <li key={text} className={styles['top-level-item']}>
+      <a href={url} className={styles['top-level-item-link']}>
+        {text}
+      </a>
+    </li>
+  );
 
   listItems.unshift(
-    <li className={styles['logo']} />
-  )
+    <li className={styles['logo-container']}>
+      <a className={styles.logo} href="/"/>
+    </li>
+  );
+
   return (
     <nav>
       <ul className={styles.nav}>
