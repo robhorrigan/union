26a9771e54080e4088c2065754e23049e582d73c
dmitrinesterenko <dneseterenko@xogrp.com>
2017-04-07 18:34:39 -0400

    Adds a Cloudfront invalidation after the S3 copy

--- scripts/publish-assets.sh
+++ scripts/publish-assets.sh
@@ -1,5 +1,10 @@
 s3_uri=s3://xo-union
-
+distribution_id=$1
+if [ -z "$distribution_id" ]; then
+    echo "Please provide a Cloudfront distribution ID as the first parameter"
+    exit 1
+fi
 npm install \
 && npm run build.patterns \
-&& aws s3 cp ./public/assets $s3_uri --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+&& aws s3 cp ./public/assets $s3_uri --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
+&& aws cloudfront create-invalidation --distribution-id $distribution_id --paths /public/assets
