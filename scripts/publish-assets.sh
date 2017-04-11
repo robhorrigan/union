7c194e368e743109ae444dd4c3ad9a07c6309289
George Taveras <george.taveras1231@gmail.com>
2017-04-11 09:05:31 -0400

    Create cache invalidation when deploying docs

--- scripts/publish-assets.sh
+++ scripts/publish-assets.sh
@@ -2,5 +2,5 @@ eval $(gpg --decrypt ./scripts/secrets/hidden/publish-env.sh)
 
 npm install \
 && npm run build.patterns \
-&& aws s3 cp ./public/assets $S3_ASSETS_BUCKET_URI --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
-&& aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ASSETS_DISTRIBUTION_ID --paths /public/assets
+&& aws s3 cp ./public/assets $ASSETS_BUCKET_URI --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
+&& aws cloudfront create-invalidation --distribution-id $ASSETS_DISTRIBUTION_ID --paths /public/assets
--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -1,5 +1,6 @@
-s3_uri=s3://docs.union.theknot.com
+eval $(gpg --decrypt ./scripts/secrets/hidden/publish-env.sh)
 
 npm install \
 && NODE_ENV=production npm run build \
-&& aws s3 cp ./public/docs $s3_uri --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+&& aws s3 cp ./public/docs $DOCS_BUCKET_URI --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+&& aws cloudfront create-invalidation --distribution-id $DOCS_DISTRIBUTION_ID --paths /public/assets
