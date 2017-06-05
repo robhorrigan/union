4fe476251d5c1061982a5bad0256ff9c1578bac0
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2017-06-05 12:56:33 -0400

    [skip ci] Add bootstrap command to publish docs script (#161)

--- scripts/publish-docs.sh
+++ scripts/publish-docs.sh
@@ -7,6 +7,7 @@ fi
 
 yarn install \
 && yarn run clean.public \
+&& yarn bootstrap \
 && NODE_ENV=production yarn build \
 && aws s3 cp ./public/docs $DOCS_BUCKET_URI --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
 && aws cloudfront create-invalidation --distribution-id $DOCS_DISTRIBUTION_ID --paths '/*'
