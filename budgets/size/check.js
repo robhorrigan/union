d359a8f331ea9a217b5048e7ec7ffd7dee6603da
George Taveras <george.taveras1231@gmail.com>
2017-06-27 19:13:58 -0400

    Add budget.size.check and budget.size.update script

---
+++ budgets/size/check.js
@@ -0,0 +1,57 @@
+import getCurrentStats from './interactiveUpdateBudget/getCurrentStats';
+import { getCurrentBudget, setBudget } from './interactiveUpdateBudget/manageBudget';
+import formattedPercentDiff from './interactiveUpdateBudget/formattedPercentDiff';
+
+function test(actual, expected) {
+  return {
+    pass: expected.size > actual.size,
+    diff: formattedPercentDiff(expected.size, actual.size)
+  };
+}
+
+async function checkBudget() {
+  const [currentStats, currentBudget] = await Promise.all([
+    getCurrentStats(), getCurrentBudget()
+  ]);
+
+  const result = {
+    passed: true,
+    failures: []
+  };
+
+  for (const stat of currentStats) {
+    const budget = currentBudget[stat.name];
+
+    const { pass, diff } = test(stat, budget);
+
+    if (!pass) {
+      result.passed = false;
+      result.failures.push({
+        name: stat.name,
+        diff
+      });
+    }
+
+    console.log(stat.name, diff);
+  }
+
+  return result;
+}
+
+
+checkBudget()
+.then(({ passed, failures }) => {
+  if (!passed) {
+    console.error('\nSome budgets were exceeded\n\n');
+
+    for (const { name, diff } of failures) {
+      console.error(name, diff);
+    }
+
+    process.exit(1);
+  }
+})
+.catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
